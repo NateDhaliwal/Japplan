cad6e054a9e5eeb8998892565e70b64004411201
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2024-12-29 11:09:24 +0000

    Create base layout

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let textbold = document.getElementById('textbold')
-let textcolor = document.getElementById('textcolor')
-let textarea = document.getElementById('textarea')
+let textbold = document.getElementById('textbold');
+let textcolor = document.getElementById('textcolor');
+let textarea = document.getElementById('textarea');
 
-textbold.addEventListener('close', function() {
-  textarea.style.b
+textbold.addEventListener('click', function() {
+  if (textarea.style.fontWeight === 'bold') {
+    textarea.style.fontWeight = 'normal';
+  } else {
+    textarea.style.fontWeight = 'bold';
+  }
+});
+
+textarea.addEventListener('change', function(){
+  localStorage.setItem('Japplan', textarea.value);
 })
